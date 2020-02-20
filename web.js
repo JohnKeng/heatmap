c1282041e6999986dbbaa3f42998e0f3b9cfcece
John Keng <kn@6-17.net>
2020-02-20 20:10:31 +0800

    Add files via upload

--- web.js
+++ web.js
@@ -1,103 +1,91 @@
-const body = document.body
-const heatmap = h337.create({
-  container: document.getElementById('heatmapContainer'),
-  maxOpacity: .6,
-  radius: 40,
-  blur: .8
-})
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var path = require('path');
+
+// var nStatic = require('node-static');
+// var server = new nStatic.Server('./');
+
+		
+		
 
 
-// 這段是隨機九個點的矩陣 兩個矩陣 3^2 = 9 個點 （你的程式用不到可刪掉）
-const X = [313.5,500,687.5]
-const Y = [113.5,300,487.5]
-Array.prototype.ram = function(){
-  return this[(Math.random()*this.length) >> 0]
-}
+http.createServer(function(request, response){
+	  console.log('Connection');
+  var info = url.parse(request.url)
+  console.log(info.pathname)
+  switch (info.pathname) {
+    //root path, show page
+  	case '/':
+  	  showPage(response);
+  	  break;
+    //path for arduino, new data will be sent to this route
+  	// case '/record':
+    //   //data list
+  	//   var postData = [];
+  	//   request.on('data', (data) => {
+  	//   	postData.push(data);
+  	//   }).on('end', () => {
+    //     //combine data to a string
+		// console.log(postData[0]);
+  	//   	var jsonStr = Buffer.concat(postData).toString().trim();
+    //     if(jsonStr) { //if data isn't empty
+    //       //parse data string to json
+  	//   	  heatmapData = JSON.parse(jsonStr);
+		//   console.log(heatmapData);
+    //       //clear data
+    // 	  postData = [];
+    //       //sent response to long polling requests
+    //       connections.forEach((res) => {
+    //         res.end(JSON.stringify(heatmapData));
+    //       });
+    //       //clear long polling requests
+    // 	  	connections = [];
+    //     } else { //data is empty
+    //       connections.forEach((res) => {
+    //         res.end("clear");
+    //       });
+    //     }
+  	//   	response.end('ok');
+  	//   });
+  	//   break;
+    //long polling path
+  	// case '/update':
+    //   //save response for long polling use
+  	// 	connections.push(response);
+    //   break;
+    // //will be called when the page first load
+    // //return the current data
+    // case '/get_current':
+    //   response.end(JSON.stringify(heatmapData));
+    //   break;
+    default:
+      //get icon image
+      if(path.extname(info.pathname) == ".jpg") {
+        fs.readFile(info.pathname, (err, file) => {
+          response.write(file);
+          response.end();
+        });
+      } else if(path.basename(info.pathname) == 'heatmap.js') {
+        //get heatmap.js
+		console.log("requiring heatmap.js");
+        fs.readFile('./heatmap.js', (err, js) => {
+          response.write(js);
+          response.end();
+        });
+      }
+  }
 
+}).listen(8080);
 
-
-
-
-// 畫熱點以及框線 or 白點
-function g(){
-
-  // 隨機選一個點
-  const x = X.ram()
-  const y = Y.ram()
-  console.log(x, y)
-
-  // 熱點 value +1
-  heatmap.addData({ x: x, y: y, value: 1 })
-
-  // 你要的畫框 function
-  dot() // 白點
-  rect() // 外框
-  line() // 井字線
-}
-
-
-
-// 模擬熱點用的，滑鼠移動就 +1 （你的程式用不到可刪掉）
-body.onmousemove = (e) => {
-  e.preventDefault()
-  g()
-}
-// 網頁一打開先模擬30個熱點 （你的程式用不到可刪掉）
-for(let i=0; i<30; i++){
-  const rand = (Math.random() * 1500) >> 0
-  setTimeout(g, rand)   
-}
-
-
-
-
-
-
-// 畫圖的主要函示 
-function cins(){
-  const c = document.getElementsByClassName("heatmap-canvas");
-  return c[0].getContext("2d");
-}
-// 點
-function dot(){
-  const ctx = cins()
-  ctx.fillStyle = "white"
-  ctx.fillRect(308.5,108.5,10,10)
-  ctx.fillRect(308.5,295,10,10)
-  ctx.fillRect(308.5,482.5,10,10)
-  ctx.fillRect(495,108.5,10,10)
-  ctx.fillRect(495,295,10,10)
-  ctx.fillRect(495,482.5,10,10)
-  ctx.fillRect(682.5,108.5,10,10)
-  ctx.fillRect(682.5,295,10,10)
-  ctx.fillRect(682.5,482.5,10,10)
-}
-// 框
-function rect(){
-  const ctx = cins()
-  ctx.beginPath()
-  ctx.lineWidth = "5";
-  ctx.strokeStyle = "#5f5fe0"
-  ctx.rect(220, 20, 561, 561)
-  ctx.stroke()
-}
-// 井
-function line(){
-  const ctx = cins()
-  ctx.strokeStyle = "#5f5fe0"
-  ctx.beginPath()
-
-  ctx.moveTo(407, 20)
-  ctx.lineTo(407, 580)
-  ctx.stroke()
-  ctx.moveTo(594, 20)
-  ctx.lineTo(594, 580)
-  ctx.stroke()
-
-  ctx.moveTo(220, 207)
-  ctx.lineTo(780, 207)
-  ctx.stroke()
-  ctx.moveTo(220, 394)
-  ctx.lineTo(780, 394)
-  ctx.stroke()
+//main page
+function showPage(response) {
+  response.writeHead(200, {'Content-Type': 'text/html'}); 
+  var filename = "index.html";  //這裡有跑html程式!
+  //get html file
+  fs.readFile(filename, (err, html) => {
+    //send html
+  	response.write(html);
+  	response.end();
+  });
 }
